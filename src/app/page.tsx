0ae2710fdf13c0874855366ef1bbd8db561d8c29
izzatPro <108723105+izzatPro@users.noreply.github.com>
2024-01-05 12:48:21 +0500

    Starter bookmodule

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import {Button} from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h1>SheyEvents</h1>
+      <Button color='secondary' variant='solid' size="sm">Cick me</Button>
+    </div>
+
+  )
+}
